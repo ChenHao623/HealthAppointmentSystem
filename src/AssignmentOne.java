301a13cbaf06339bbc9221ec4425c4bd5f6911ba
ChenHao623 <h.chen.44@student.scu.edu.au>
2024-11-19 17:32:15 +0800

    Update AssignmentOne.java
    Added create and print appointment logic in main method

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -30,5 +30,52 @@ public class AssignmentOne {
         dietitian1.printDetails();
         dietitian2.printDetails();
         System.out.println("------------------------------");
+
+        // Part 5 – Collection of appointments
+        ArrayList<Appointment> appointments = new ArrayList<>();
+        createAppointment(appointments, "James Bond", "1234567890", "08:00", gp1);
+        createAppointment(appointments, "Tony Stark", "1112233445", "14:30", gp2);
+        createAppointment(appointments, "Sherlock Holmes", "0987654321", "10:00", dietitian1);
+        createAppointment(appointments, "Bruce Wayne", "2233445566", "12:00", dietitian2);
+        printExistingAppointments(appointments);
+        System.out.println("------------------------------");
+    }
+
+    /**
+     * Creates a new appointment and adds it to the list of appointments.
+     * If the input details are valid, the appointment will be created and added to the list.
+     *
+     * @param appointments The list of appointments to which the new appointment will be added.
+     * @param patientName The name of the patient.
+     * @param patientMobile The mobile number of the patient.
+     * @param timeSlot The time slot for the appointment.
+     * @param doctor The doctor assigned to the appointment.
+     */
+    public static void createAppointment(ArrayList<Appointment> appointments, String patientName,
+                                         String patientMobile, String timeSlot, HealthProfessional doctor) {
+        if (patientName != null && patientMobile != null && timeSlot != null && doctor != null) {
+            Appointment appointment = new Appointment(patientName, patientMobile, timeSlot, doctor);
+            appointments.add(appointment);
+            System.out.println("Create appointment successfully for " + patientName);
+        } else {
+            System.out.println("Failed to create appointment.");
+        }
+    }
+
+    /**
+     * Prints all the existing appointments in the list.
+     * If there are no appointments, a message will be printed to indicate this.
+     *
+     * @param appointments The list of appointments to print.
+     */
+    public static void printExistingAppointments(ArrayList<Appointment> appointments) {
+        if (appointments.isEmpty()) {
+            System.out.println("No existing appointments.");
+        } else {
+            for (Appointment appointment : appointments) {
+                appointment.printDetails();
+                System.out.println();
+            }
+        }
     }
 }
