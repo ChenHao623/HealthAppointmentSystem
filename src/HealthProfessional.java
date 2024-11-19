797836581c7de1dd5eb8fc75064e2a025fc56952
ChenHao623 <h.chen.44@student.scu.edu.au>
2024-11-19 17:01:03 +0800

    Update HealthProfessional.java
    Added HealthProfessional base class with properties and methods。

---
+++ src/HealthProfessional.java
@@ -0,0 +1,39 @@
+/**
+ * Represents a health professional with basic details such as ID, name, and additional info.
+ * This is the base class that can be extended by specific types of health professionals
+ * like General Practitioners, Dietitians, etc.
+ */
+public class HealthProfessional {
+    protected int id;
+    protected String name;
+    protected String basicInfo;
+
+    /**
+     * Default constructor to initialize a HealthProfessional object with default values.
+     */
+    public HealthProfessional() {
+    }
+
+    /**
+     * Constructor to initialize a HealthProfessional object with specific values for ID, name, and basic info.
+     *
+     * @param id The ID of the health professional.
+     * @param name The name of the health professional.
+     * @param basicInfo The basic information of the health professional.
+     */
+    public HealthProfessional(int id, String name, String basicInfo) {
+        this.id = id;
+        this.name = name;
+        this.basicInfo = basicInfo;
+    }
+
+    /**
+     * Prints the details of the health professional.
+     * This includes the ID, name, and basic info.
+     */
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Basic Info: " + basicInfo);
+    }
+}
